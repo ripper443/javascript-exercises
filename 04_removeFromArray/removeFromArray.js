9bc0d748dd5f9738dd66c603a03faeaad3030c2b
ripper443 <bobd7590@gmail.com>
2022-08-02 01:36:14 -0400

    added ideas

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -4,3 +4,7 @@ const removeFromArray = function() {
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+//use splice() to remove elements in an array
+// use spread operator / rest
