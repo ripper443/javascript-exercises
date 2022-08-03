c19f56071d3f60b970a9b1265119b5a4a524b83e
ripper443 <bobd7590@gmail.com>
2022-08-03 05:46:48 -0400

    added more ideas

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -8,3 +8,118 @@ module.exports = removeFromArray;
 
 //use splice() to remove elements in an array
 // use spread operator / rest
+
+/** 
+ * 1. takes an array from 1st argument 
+ * 2. check if 2nd argument is in 1st arg  ; use rest operator
+ * 3. remove from array list
+ */
+
+ const removeFromArray2= function(arr , ...args) {
+
+};
+
+
+if arg == arr {
+    Array.splice(arr[i], 1 )
+}
+
+// get index number of args and store in i
+// args is an array
+
+arr = [1,2,3,4,5];
+args = 3;
+
+arr.splice(/**index number for arr */,/**args.length */)
+
+//check if args == arr , get index number
+
+
+    //first scenario
+arr = [1,2,3,4,5];
+args = [ 3 ];
+ 1. loop through args;
+ 2. if its a Number, store in a var , get index num in arr
+ 3. if its !Number return arr 
+
+
+    //second scenario
+arr = [1,2, "randomization" , "randomness"];
+args = [ "randomness" , "randomization"];
+
+1. if arr[i] == args[i] , get indexOf ; //////YES
+
+
+arr = [1,2, "randomization" , "randomness"];
+args = [ 1, 45 , "randomness" , "randomization"];
+
+same = [];
+another = [];
+for (let i = 0; i <= arr.length -1 ; i ++) {
+
+    for (let j = 0; j<= args.length -1; j++){
+
+      if (args[j] === arr[i]) {
+       same.push(args[j]);
+      }
+   
+}
+};  for (let i = 0 ; i <=same.length-1; i ++){
+    another.push(arr.indexOf(same[i]));
+ }; ///another now stores indexes that can be removed using splice()
+
+ for (let i = 0; i <= arr.length -1 ; i ++){
+    arr.splice(another[i] , 1);
+ } ; console.log(arr);
+ 
+ arr.splice(/**index number for arr */,/**args.length */)
+
+
+
+
+
+ arr = [1,22, "three" , "five"];
+ another = [ 0 , 1 , 2];
+
+ for (let y = 0; y <= another.length -1 ; y ++){
+    arr.slice(another[y])
+ } ; console.log(arr);
+
+
+ //notes : maybe its better if index is obtained AFTER splice  :)
+
+ arr = [1,22, "three" , "five"];
+
+ arr.splice
+
+ arr[1];
+
+
+
+
+
+
+
+ //do i know everything about variables????
+
+
+
+//need to find and store index numbers
+indexxed.push(arr.indexOf(same[i]));
+
+same = [1,"randomness" , "randomization"];
+
+for (let i = 0 ; i <=same.length-1; i ++){
+   another.push(arr.indexOf(same[i]));
+}
+
+
+let i = arr.indexOf(numstr);
+
+
+let ind = arr.indexOf(args);
+ind;
+
+
+
+
