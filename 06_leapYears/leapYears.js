923ff677fb3e4c2cc6b17df0df898d8f0d65e307
ripper443 <bobd7590@gmail.com>
2022-08-05 22:19:35 -0400

    completed exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,28 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 100  && year % 400 ) {
+            return true;
+        } else if (year % 100 ===0 && year % 400===0 ){
+            return true;
+         } else {
+            return false;
+        }
+            }
+     else {
+        return false;
+    };}
 
 // Do not edit below this line
 module.exports = leapYears;
+
+/** 
+if (!(year % 4)) {
+    if (!(year % 100 && year % 400)) {
+        return true;
+    } else {
+        return false;
+    }
+
+} else {
+    return false;
+}; **/
