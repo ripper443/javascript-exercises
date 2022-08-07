83b2014afd204a01710b1bb905f928062d297910
ripper443 <bobd7590@gmail.com>
2022-08-07 07:12:38 -0400

    completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+const ftoc = function(n) {
+  let c =(n-32) * 5 / 9;
 
-};
+  let rounded = Math.round(c *10) /10;
+  return rounded;
+}; 
 
-const ctof = function() {
-
+const ctof = function(n) {
+  let f = (n * 9/5) + 32 ;
+  let rounded = Math.round(f * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
@@ -11,3 +16,15 @@ module.exports = {
   ftoc,
   ctof
 };
+
+
+//Formula
+//(n°F − 32) × 5/9 = n°C
+
+/** 
+function test(n) {
+  let c =(n-32) * 5 / 9;
+
+  let result = Math.round(c *10) /10;
+  return result;
+} ;  test(100); **/
